44c6f013e0a7c6deb4f2ba7cff52f280e587cfb4
wangzhengping <wangzhengping@kezaihui.com>
2023-05-09 19:22:05 +0800

    优化相关函数

--- lib/src/data/link.dart
+++ lib/src/data/link.dart
@@ -1,8 +1,13 @@
+import 'dart:ui';
+
 class Link<T> {
   final T source;
   final T target;
   int index = 0;
   num weight = 0;
+
+  ///存储位置
+  final List<Offset> points = [];
 
   Link(this.source, this.target);
 }
